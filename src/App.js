5dcdb76898417873ce2b2d8e69b7c095315e3a3e
aubreyjustine713 <aubreyjustinejamison@gmail.com>
2023-08-14 19:54:21 -0400

    testing to try to deploy site on netlify

--- src/App.js
+++ src/App.js
@@ -6,9 +6,24 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded with 🖤 by <a href="">Aubrey Jamison</a> and is{" "}
-        <a href="">open sourced on GitHub</a>
+        This project was coded with 🖤 by{" "}
+        <a
+          href="https://www.linkedin.com/in/aubreyjamison713/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aubrey Jamison
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/aubreyjustine713/weather-react-app-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on GitHub
+        </a>
       </footer>
+      <p>test</p>
     </div>
   );
 }
