669125535cc31a82dfe386d777ecf903dc0b55d6
aubreyjustine713 <aubreyjustinejamison@gmail.com>
2023-08-14 20:25:05 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded with 🖤 by{" "}
-        <a
-          href="https://www.linkedin.com/in/aubreyjamison713/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Aubrey Jamison
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/aubreyjustine713/weather-react-app-2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
-      <p>test</p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded with 🖤 by{" "}
+          <a
+            href="https://www.linkedin.com/in/aubreyjamison713/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Aubrey Jamison
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/aubreyjustine713/weather-react-app-2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
