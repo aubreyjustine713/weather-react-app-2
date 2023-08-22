8276672f873f98bfe8bc6f8b8666d8273e7fdaee
aubreyjustine713 <aubreyjustinejamison@gmail.com>
2023-08-21 20:55:56 -0400

    added coordinates shift

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
   function handleResponse(response) {
     console.log(response.data);
   }
+  console.log(props);
   let apiKey = "d8ae07eb683be1d16aad578147a0ceef";
-  let longitude = 40.7;
-  let latitude = 74;
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
   return (
